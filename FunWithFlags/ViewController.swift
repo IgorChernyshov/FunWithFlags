04d99a35a73de47542aaf3fb4d5239766ce42e0c
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-06-20 18:08:15 +0300

    Initial Commit

---
+++ FunWithFlags/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunWithFlags
+//
+//  Created by Igor Chernyshov on 20.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
